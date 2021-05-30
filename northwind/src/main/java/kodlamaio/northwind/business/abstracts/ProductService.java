8882dc30c1fbf94f4fff4142dabe50b873f51e9c
Tülin Aktaş <tulin-aktas@hotmail.com>
2021-05-31 00:42:04 +0300

    Dto and ExceptionHandler

--- northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 public interface ProductService {
 	DataResult<List<Product>> getAll();
@@ -18,4 +19,5 @@ public interface ProductService {
 	DataResult<List<Product>> getByProductNameContains(String productName);
 	DataResult<List<Product>> getByProductNameStartsWith(String productName);
 	DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId);
+	DataResult<List<ProductWithCategoryDto>> getProductWithCategoryDetails();
 }
